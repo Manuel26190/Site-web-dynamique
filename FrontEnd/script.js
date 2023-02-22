f07055b78bf4155d8b36fe8354739bada0f0c22f
Manuel Faure <manuelfaure26190@gmail.com>
2023-02-22 09:54:07 +0100

    save p3

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,4 @@
-const figure1 = document.querySelector('.figure1');
+
 const figure2 = document.querySelector('.figure2');
 const figure3 = document.querySelector('.figure3');
 const figure4 = document.querySelector('.figure4');
@@ -7,18 +7,23 @@ const figure6 = document.querySelector('.figure6');
 const figure7 = document.querySelector('.figure7');
 const figure8 = document.querySelector('.figure8');
 const figure9 = document.querySelector('.figure9');
+const figure10 = document.querySelector('.figure10');
+const figure11 = document.querySelector('.figure11');
+
+const figure = document.querySelector('.figure1')
 
 fetch('http://localhost:5678/api/works')
 .then ((response) => response.json())
 .then ((data) => {
          
     const img1 = document.createElement("img");
-    img1.src = data[0].imageUrl;
-    figure1.appendChild(img1);
-
+    img1.innerHTML = img1.src = data[0].imageUrl;
+    figure.appendChild(img1);  
+    
     const img2 = document.createElement("img");
-    img2.src = data[1].imageUrl;
+    img2.innerHTML = img2.src = data[1].imageUrl;
     figure2.appendChild(img2);
+    
 
     const img3 = document.createElement("img");
     img3.src = data[2].imageUrl;
@@ -47,6 +52,14 @@ fetch('http://localhost:5678/api/works')
     const img9 = document.createElement("img");
     img9.src = data[8].imageUrl;
     figure9.appendChild(img9);
+
+    const img10 = document.createElement("img");
+    img10.src = data[9].imageUrl;
+    figure10.appendChild(img10);
+
+    const img11 = document.createElement("img");
+    img11.src = data[10].imageUrl;
+    figure11.appendChild(img11);
 
     
 
