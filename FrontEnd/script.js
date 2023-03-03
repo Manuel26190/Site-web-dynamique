ac4e233540346f894df4881f265f47fb0ff6d8d0
Manuel Faure <manuelfaure26190@gmail.com>
2023-03-03 11:40:58 +0100

    save p3

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,14 +5,16 @@ fetch('http://localhost:5678/api/works')
 .then ((response) => response.json())
 
 //function qui génére les fiches projets//
-.then ((data) => {
-    for (let i = 0; i < data.length; i++) {   
-        const article = data[i]; 
+.then ((values) => {
+    //console.log(data[0].title);
+    for (let i = 0; i < values.length; i++) {   
+        const article = values[i];
+        //console.log(article.categoryId == 2); 
 
         const fiches = document.createElement("fiches");
 
         const img = document.createElement("img");
-        img.innerHTML = img.src = data[i].imageUrl;
+        img.innerHTML = img.src = values[i].imageUrl;
 
         const figcaption = document.createElement("figcaption");
         figcaption.innerHTML = article.title;    
@@ -22,27 +24,73 @@ fetch('http://localhost:5678/api/works')
         gallery.appendChild(fiches);
     }
 
-    //Trier et filtrer //
-    //filtre Objets          
-    const boutonObjets = document.querySelector('#btnObjets');
-    const categoryObjets = boutonObjets.dataset.categoryId;
-    console.log(categoryObjets);
-    console.log(boutonObjets);
+    // Création d'une fonction pour filtrer les objets en fonction du bouton de catégorie cliquer sur le site
+    function filterObjets(values, categoryId) {
+
+        let filteredValues;
+
+// Création de l'option "par défaut" pour dire que SI aucune catégorie n'est choisi (en cliquant sur le bouton 'tous'
+// alors tout les éléments apparraitront à l'écran)
+        if (categoryId.length === 0) {
+            filteredValues = values;
+
+        }else{ //Sinon, on filtrera les objets pour afficher seulement ceux dont la catégorie est mentionner au click boutton
+            filteredValues = values.filter(value => categoryId.includes(value.categoryId));
+        }
+
+        // On vide les éléments HTML présent dans Gallery
+        let gallery = document.querySelector(".gallery");
+        gallery.innerHTML = "";
+
+// Itération sur les objets filtrés pour créer les éléments HTML ci-joints
+        filteredValues.forEach( value => {
+
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let figcaption = document.createElement("figcaption");
+
+        img.setAttribute("src", value.imageUrl);
+        figcaption.setAttribute("alt", value.title);
+        img.setAttribute("crossorigin", "anonymous");
+
+        figcaption.innerHTML = value.title;
+
+        figure.append(img, figcaption);
+        gallery.append(figure);
+
+});
+}
+// Création d'événements "au click" sur différent bouton pour filtrer les éléments selon leur catégorie (bouton) séléctionné
+const noFilter = document.querySelector("#btnTous");
+noFilter.addEventListener("click", function() {
+    filterObjets(values, []);
+});
+
+const boutonObjets1 = document.querySelector("#btnObjets");
+boutonObjets1.addEventListener("click", function() {
+    filterObjets(values, [1]);
+});
+
+const boutonObjets2 = document.querySelector("#btnAppartements");
+boutonObjets2.addEventListener("click", function() {
+    filterObjets(values, [2]);
+});
+
+const boutonObjets3 = document.querySelector("#btnHhotels");
+boutonObjets3.addEventListener("click", function() {
+    filterObjets(values, [3]);
+});
 
     
-})
+        
+    
+     
+    
+});
 
 
 
 
-//boutonObjets.addEventListener('click', function (e) {
-        //console.log('event %o',e);        
-        //const projetsFiltres = data
-            //.filter (function (data) {
-            //return data.categoryId == categoryObjets;            
-        //})        
-    //gallery.innerHTML = projetsFiltres;           
-    //});   
 
     
  
