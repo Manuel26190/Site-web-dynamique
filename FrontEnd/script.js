deddfb623f86360dbfe0006b00a7c6e1ab7da895
Manuel Faure <manuelfaure26190@gmail.com>
2023-03-13 14:44:07 +0100

    save p3

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -100,47 +100,44 @@ if (values === null){
 
 //Envoi du formulaire Login
 
-//je cible mon formulaire login 
-const myForm = document.querySelector('#myForm');
+// Je cible le formulaire dans la page HTML
+const form = document.querySelector('#myForm');
 
-
-//Je fais un écouteur d'événement au submit de mon formulaire
-myForm.addEventListener('submit', async function (event) {   
+// Évènement au "submit" du formulaire de connexion
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    // Récupération des champs du formulaire nécessaire pour la requête API    
+    // Récupération des champs du formulaire nécessaire pour la requête API
     let adminInfo = {
-        email: myForm.elements.eMail.value,
-        password: myForm.elements.password.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value
     };
-        console.log(adminInfo);
 
     // Requête POST asynchrone à l'API Swagger avec les informations nécessaire pour l'identification
     const response = await fetch("http://localhost:5678/api/users/login", {
         method: 'post',
         headers: {
-            'content-type' : 'application/json'
+            'content-Type': 'application/json'
         },
         body: JSON.stringify(adminInfo)
     });
-    
-});
+    // Ce qu'il se passe si la requête envoyer a l'API est du statut 200 : D'abord récuperer les données reçues par l'API au format JSON
+    if (response.ok) {
+        const data = await response.json();
 
-// Ce qu'il se passe si la requête envoyer a l'API est du statut 200 : D'abord récuperer les données reçues par l'API au format JSON
-if (response.ok) {
-    const data = response.JSON();
-    
-    // Enregistrement du Token dans le local storage afin de pouvoir s'en servir pour la suite du Projet
-    sessionStorage.setItem('token', data.token);
-    
+        // Enregistrement du Token dans le local storage afin de pouvoir s'en servir pour la suite du Projet
+        sessionStorage.setItem('token', data.token);
 
-    // Rediriger l'utilisateur sur la page d'accueil
-    window.location.href = 'index.html'
+        // Rediriger l'utilisateur sur la page d'accueil
+        window.location.href = 'index.html'
 
-} else { 
-    alert ('Erreur dans l’identifiant ou le mot de passe');
-
-};
+    } else { // Si requête négatif (statut autre que 200), envoyer un message d'alerte sur l'écran
+        alert("Identifiant ou mot de passe incorrect");
+    }
+})
+    /*.catch(error => { // Code pour les autres problèmes que l'on pourrait rencontrer 
+        console.error('erreur:', error);
+    });*/
 
 
 
