119d5bc5b135477b5ee7fe59c24e50ffde465233
Manuel Faure <manuelfaure26190@gmail.com>
2023-03-16 15:46:41 +0100

    save p3

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -99,5 +99,39 @@ let token = sessionStorage.getItem("token");
  
 //Ouverture de la fenêtre modale
 
+//Variable qui sert à fermer la boite modale
+let modal = null;
+
+//Foinction qui cible mes lien href et annule le display block pour faire apparaître la fenêtre modale
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector (e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden')
+    target.setAttribute ('aria-modal', 'true')
+    modal = target 
+    modal.addEventListener ('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)  
+    
+};
+ //Fonction qui ferme la modale
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault()    
+    modal.style.display = 'none';
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute ('aria-modal')
+    modal = null
+
+    
+    modal.removeEventListener ('click', closeModal)
+}
+
+//Création de l'événement au click sur les lien <a>
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal)   
+    
+});
+
 
  
