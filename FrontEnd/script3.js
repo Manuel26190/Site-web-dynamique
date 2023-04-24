2a0f19ce0f313710c2fdd900f58e70d46db209d5
Manuel Faure <manuelfaure26190@gmail.com>
2023-04-24 11:53:50 +0200

    save p3

--- FrontEnd/script3.js
+++ FrontEnd/script3.js
@@ -1,46 +1,49 @@
-
-
 const imageUrl = document.getElementById('input-file');
-const title = document.getElementById('photoTitle') ;   
-const select = document.getElementById('categoryList');
+const title = document.getElementById('photoTitle'); 
 
 const btnValidModal2 = document.getElementById('btnValider');
 
-
-function CheckForm (valid){
-    if ( title.value !== "" || imageUrl.files.length === 0) {
+//Fonction qui vérifie que les champs ajout de photo et titre du formulaire soient bien rempli 
+function CheckForm (el, el2, valid){
+    if ( el.value !== "" || el2.files.length === 0) {
         valid.disabled = false;
     } else {
         valid.disabled = true;
     }
 };
 
-
+//Fonction qui vérifie si l'utilisateur entre des données dans le champs titre du fomulaire
 title.addEventListener("input", () => {
-    CheckForm(btnValidModal2)
+    CheckForm(title, imageUrl, btnValidModal2)
 });
+
+//Fonction qui vérifie si l'utilisateur entre des données dans le champs ajout d'image du fomulaire
 imageUrl.addEventListener("input", () => {
-    CheckForm(btnValidModal2)
-});
-
-
-
-
-
+    CheckForm(title, imageUrl, btnValidModal2)
+}); 
 
 const formEl = document.getElementById('pictureForm');
 
+//Fonction qui vérifie que les tout champs du fomulaire soient bien rempli 
+//et qui envoie les donnéées du formulaire vers l'API au click du bouton valider
 formEl.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    if (!title.value || !imageUrl.files || !select.value) {
+    const category = document.getElementById('categoryList');
+
+    if (!title.value || !imageUrl.files || !category.value) {
         alert("Veuillez remplir toutles les champs du fomrulaire.");
         return;
-    }
+    }    
 
-    const formData = new FormData(formEl);
-    const data = new URLSearchParams(formData);
-    let dataObj = Object.fromEntries(formData);
+    const formData = new FormData();
+    formData.append('image-file', imageUrl.files[0]);
+    formData.append('title', title.value);
+    formData.append('category', category.value);
+
+    //const data = new URLSearchParams(formData);
+    //let dataObj = Object.fromEntries(formData);
+
     let apiObj = {
         "id": 0,
         "title": "Bar New-York",
@@ -49,7 +52,7 @@ formEl.addEventListener('submit', function (e) {
         "userId": 0
       };
 
-    for (let entry of data) {
+    for (let entry of formData) {
         console.log(entry);
     }
 
@@ -58,27 +61,23 @@ formEl.addEventListener('submit', function (e) {
         headers: {
             'Authorization': 'bearer' + sessionStorage.getItem("token"),
         },
-        body: JSON.stringify(apiObj)
+        body: formData
     }).then(res => res.json())
       .then(data => console.log(data))
       .catch(error => console.log(error));      
 });
 
 //function qui upload l'image et la fait apparaître dans la modale
-
-const inputFile = document.getElementById('input-file');
-
-inputFile.onchange = function (){
+imageUrl.onchange = function (){
 
     const profilePicture = document.getElementById('profilePicture');    
     const labelFile = document.getElementById('labelFile');
     const imgSize = document.getElementById('imgSize');
 
-    profilePicture.src = URL.createObjectURL(inputFile.files[0])
+    profilePicture.src = URL.createObjectURL(imageUrl.files[0])
     profilePicture.style.width = '129px'; 
-    profilePicture.style.height = '169px';
-    //profilePicture.style.marginTop ='28px'; 
-    inputFile.innerHTML = "";          
-    labelFile.innerHTML = "";    
-    imgSize.innerHTML = "";
+    profilePicture.style.height = '169px';    
+    imageUrl.remove();          
+    labelFile.remove();    
+    imgSize.remove();
 };
